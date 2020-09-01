b41a6b3cd0c49aa790fdfdcfc189b56988f2c2f2
Le Duc Anh <ledcuanh.bkit10@gmail.com>
2020-09-01 15:54:06 +0700

    first commit

---
+++ OpenPlayerDemo/src/main/java/com/audionowdigital/android/openplayerdemo/MainActivity.java
@@ -0,0 +1,201 @@
+package com.audionowdigital.android.openplayerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.audionowdigital.android.openplayer.Player;
+import com.audionowdigital.android.openplayer.Player.DecoderType;
+import com.audionowdigital.android.openplayer.PlayerEvents;
+
+
+// This activity demonstrates how to use JNI to encode and decode ogg/vorbis audio
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity ";
+
+
+
+    private TextView logArea;
+
+    private EditText urlArea;
+
+    private Button initWithFile;
+
+    private Button initWithUrl;
+
+    private Button play;
+
+    private Button pause;
+
+    private Button stop;
+
+    private SeekBar seekBar;
+
+    private Player.DecoderType type = DecoderType.OPUS;
+
+    private Player player;
+
+    // Playback handler for callbacks
+    private Handler playbackHandler;
+
+
+    private int LENGTH = 0;
+    // Creates and sets our activities layout
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initUi();
+
+        switch (type) {
+            case VORBIS: //urlArea.setText("http://icecast1.pulsradio.com:80/mxHD.ogg"); LENGTH = -1; break;
+                urlArea.setText("http://markosoft.ro/test.ogg");
+                LENGTH = 215;
+                break;
+            case OPUS:
+                urlArea.setText("http://www.markosoft.ro/opus/02_Archangel.opus");
+                LENGTH = 154;
+                break;
+            case MX:
+                urlArea.setText("http://stream.rfi.fr/rfimonde/all/rfimonde-64k.mp3");
+                LENGTH = -1;
+                break;
+        }
+
+        playbackHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case PlayerEvents.PLAYING_FAILED:
+                        logArea.setText("The decoder failed to playback the file, check logs for more details");
+                        break;
+                    case PlayerEvents.PLAYING_FINISHED:
+                        logArea.setText("The decoder finished successfully");
+                        break;
+                    case PlayerEvents.READING_HEADER:
+                        logArea.setText("Starting to read header");
+                        break;
+                    case PlayerEvents.READY_TO_PLAY:
+                        logArea.setText("READY to play - press play :)");
+                        break;
+                    case PlayerEvents.PLAY_UPDATE:
+                        logArea.setText("Playing:" + (msg.arg1 / 60) + ":" + (msg.arg1 % 60) + " (" + (msg.arg1) + "s)");
+                        seekBar.setProgress((int) (msg.arg1 * 100 / player.getDuration()));
+                        break;
+                    case PlayerEvents.TRACK_INFO:
+                        Bundle data = msg.getData();
+                        logArea.setText("title:" + data.getString("title") + " artist:" + data.getString("artist") + " album:" + data.getString("album") +
+                                " date:" + data.getString("date") + " track:" + data.getString("track"));
+                        break;
+                }
+            }
+        };
+
+        // quick test for a quick player
+        player = new Player(playbackHandler, type);
+
+    }
+
+    private void initUi(){
+        logArea = (TextView) findViewById(R.id.log_area);
+        urlArea = (EditText) findViewById(R.id.url_area);
+        initWithFile = (Button) findViewById(R.id.init_file);
+        initWithUrl = (Button) findViewById(R.id.init_url);
+        play = (Button) findViewById(R.id.play);
+        pause = (Button) findViewById(R.id.pause);
+        stop = (Button) findViewById(R.id.stop);
+        seekBar = (SeekBar) findViewById(R.id.seek);
+
+        initWithFile.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                logArea.setText("");
+                switch (type) {
+                    case VORBIS:
+                        player.setDataSource("/sdcard/countdown.ogg", 11);
+                        break;
+                    case OPUS:
+                        player.setDataSource("/sdcard/countdown.opus", 11);
+                        break;
+                    case MX:
+                        player.setDataSource("/sdcard/countdown.mp3", 11);
+                        break;
+                }
+            }
+        });
+
+        initWithUrl.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                logArea.setText("");
+                Log.d(TAG, "Set source:" + urlArea.getEditableText().toString());
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        player.setDataSource(urlArea.getEditableText().toString(), LENGTH);
+                    }
+                }).start();
+            }
+        });
+
+        play.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (player != null && player.isReadyToPlay()) {
+                    logArea.setText("Playing... ");
+                    player.play();
+                } else
+                    logArea.setText("Player not initialized or not ready to play");
+            }
+        });
+
+        pause.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (player != null && player.isPlaying()) {
+                    logArea.setText("Paused");
+                    player.pause();
+                } else
+                    logArea.setText("Player not initialized or not playing");
+            }
+        });
+
+        stop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (player != null) {
+                    logArea.setText("Stopped");
+                    player.stop();
+                } else
+                    logArea.setText("Player not initialized");
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    Log.d(TAG, "Seek:" + i + "");
+                    player.setPosition(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,104 @@
+# OpenPlayer for Android
+
+OpenPlayer is an Android library developed by AudioNow Digital, for playing audio in your mobile applications. You can use it for:
+
+*  Music
+*  Radio streaming
+
+It delivers support for the following codecs:
+
+*  OPUS
+*  VORBIS
+*  MP3 - by accessing the native android library Stagefright
+
+OpenPlayer delivers great performance, by using native codec implementation. It has been designed to decode OPUS and VORBIS content within an OGG Wrapper.
+
+
+# Quick start
+
+First of all, you need to add OpenPlayer to your project. If you are using Gradle, add the following line to your build.gradle file and sync your project.
+
+`compile 'com.audionowdigital:openplayer:1.0.0'`
+
+
+Using OpenPlayer to decode your media is very simple. 
+
+**1 You need to create a Handler, which will receive the media events from the library**
+
+```java
+    Handler playbackHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case PlayerEvents.PLAYING_FAILED:
+                    Log.d(TAG, "The decoder failed to playback the file, check logs for more details");
+                    break;
+                case PlayerEvents.PLAYING_FINISHED:
+                    Log.d(TAG, "The decoder finished successfully");
+                    break;
+                case PlayerEvents.READING_HEADER:
+                    Log.d(TAG, "Starting to read header");
+                    break;
+                case PlayerEvents.READY_TO_PLAY:
+                    Log.d(TAG, "READY to play - press play :)");
+                    break;
+                case PlayerEvents.PLAY_UPDATE:
+                    Log.d(TAG, "Playing:" + (msg.arg1 / 60) + ":" + (msg.arg1 % 60) + " (" + (msg.arg1) + "s)");
+                    break;
+                case PlayerEvents.TRACK_INFO:
+                    Bundle data = msg.getData();
+                    Log.d(TAG, "title:" + data.getString("title") + " artist:" + data.getString("artist") + " album:" + data.getString("album") +
+                            " date:" + data.getString("date") + " track:" + data.getString("track"));
+                    break;
+            }
+        }
+    };
+```
+
+**2 Create a `Player` instance, using the created `Handler` and the media type you want to decode**
+
+```java
+    Player player = new Player(playbackHandler, Player.DecoderType.OPUS);
+```
+
+**3 Chose the content to play:**
+
+```java
+        player.setDataSource("http://www.yourserver.ro/test_file.opus", FILE_LENGTH_SECONDS);
+```
+
+**Note:** FILE_LENGTH_SECONDS is necessary for media files, for computing progress and allowing media seek. If you are decoding a live stream, you will need to use FILE_LENGTH_SECONDS = -1.
+
+# Player Events
+OpenPlayer is an event-based library. The events that will be fired in the decoding process are the following:
+
+-  `READING_HEADER` - triggered when the library starts to read the OGG header of the stream
+-  `TRACK_INFO` - while reading the OGG Header, the library will parse track information. This event is triggered when track information is available.
+-  `READY_TO_PLAY` - called after the stream has been prepared. At this point, you can call `player.play()` in order to start playback
+-  `PLAY_UPDATE` - use this trigger for play progress
+-  `PLAYING_FAILED` - there has been a problem while decoding the stream.
+-  `PLAYING_FINISHED` - player reached the end of the stream
+
+
+# iOS Version
+OpenPlayer has also been developed for iOS platform. You can find it here:
+
+https://github.com/audionowdigital/openplayer-ios
+
+
+License
+-------
+
+	© 2014 AudioNow® Digital, LLC.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
