68eaebd405b0bc6a0fce9035f7bb3b14d33e680b
ducanh <ducanh@olli-ai.com>
2024-01-02 10:30:18 +0700

    upgrade gradle.

--- OpenPlayer/src/com/audionowdigital/android/openplayer/LogDebug.kt
+++ OpenPlayer/src/com/audionowdigital/android/openplayer/LogDebug.kt
@@ -5,22 +5,22 @@ import android.util.Log
 object LogDebug {
     @JvmStatic
     fun d(tag: String = "",msg: String){
-        if (BuildConfig.DEBUG){
+//        if (BuildConfig.DEBUG){
             Log.d("duc_anh", "$tag $msg" )
-        }
-        else {
-            Log.d("duc_anh", "$tag $msg" )
-        }
+//        }
+//        else {
+//            Log.d("duc_anh", "$tag $msg" )
+//        }
     }
 
     @JvmStatic
     fun e(tag: String = "",msg: String){
-        if (BuildConfig.DEBUG){
+//        if (BuildConfig.DEBUG){
             Log.e("duc_anh", "$tag $msg")
-        }
-
-        else {
-            Log.e("duc_anh", "$tag $msg")
-        }
+//        }
+//
+//        else {
+//            Log.e("duc_anh", "$tag $msg")
+//        }
     }
 }
